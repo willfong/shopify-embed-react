0c0a72d177b2cdf7c52913a1592a76e9d8df5361
shanhan <thecsh3@gmail.com>
2021-09-01 14:29:25 +0800

    Add app polaris frontend

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
-import { Page, Card, Button } from '@shopify/polaris';
+import { CalloutCard, DisplayText, Page, Stack } from "@shopify/polaris";
 
-function App() {
+const App = ({ client }) => {
+  let activeText = ''
+  
+  if (client && client.active) {
+    activeText = 'active 🚀'
+  } 
+  
+  if (client && !client.active) {
+    activeText = 'inactive 🚨'
+  }
+
   return (
-    <Page title="Example app">
-      <Card sectioned>
-        <Button onClick={() => alert('Button clicked!')}>Example button</Button>
-      </Card>
+    <Page>
+      <Stack vertical>
+        <DisplayText size="large">Your Referral Program is { activeText } </DisplayText>
+        <CalloutCard
+          title="Boost your referral rate"
+          illustration="https://s3.ap-southeast-1.amazonaws.com/cdn.referralcandy.com/images/shopify-embedded-app/stats.png"
+          primaryAction={{
+            content: 'View Referral Sales',
+            url: 'https://my.referralcandy.com/analytics',
+          }}
+        >
+          <p>The industry average referral rate is 2.56%. Promote more to boost your referral rate</p>
+        </CalloutCard>
+        <CalloutCard
+          title="Improve your store reach"
+          illustration="https://s3.ap-southeast-1.amazonaws.com/cdn.referralcandy.com/images/shopify-embedded-app/email.png"
+          primaryAction={{
+            content: 'Email Past Customers',
+            url: 'https://my.referralcandy.com/in_app_invite',
+          }}
+        >
+          <p>Increase the number of people who know about your program</p>
+        </CalloutCard>
+      </Stack>
     </Page>
-
   );
 }
 
